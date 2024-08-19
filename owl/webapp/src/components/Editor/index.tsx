c4a85d9a0ed635d69f6bc8257b3e675c98762939
Ceyhun Kerti <ceyhun.kerti@gmail.com>
2024-08-19 21:00:03 +0300

    active tab selector

--- owl/webapp/src/components/Editor/index.tsx
+++ owl/webapp/src/components/Editor/index.tsx
@@ -11,38 +11,63 @@ export default function EditorTabPanel() {
   const [editors, setEditors] = useState([
     { id: uuidv4(), store: createEditorStore() },
   ]);
+  const [activeTab, setActiveTab] = useState(editors[0].id);
 
   useEffect(() => {
     fetchDatabases();
   }, [fetchDatabases]);
 
+  const handleAddEditor = () => {
+    const newEditor = { id: uuidv4(), store: createEditorStore() };
+    setActiveTab(newEditor.id);
+    setEditors([...editors, newEditor]);
+  };
+
+  const handleTabChange = (tab?: string | null) => {
+    console.log(tab);
+    if (tab) setActiveTab(tab);
+  };
+
+  // return (
+  //   <Tabs defaultValue="first">
+  //     <Tabs.List>
+  //       <Tabs.Tab value="first">First tab</Tabs.Tab>
+  //       <Tabs.Tab value="second">Second tab</Tabs.Tab>
+  //       <ActionIcon variant="transparent" onClick={handleAddEditor}>
+  //         <IconPlus size={20} stroke={1} />
+  //       </ActionIcon>
+  //     </Tabs.List>
+
+  //     <Tabs.Panel value="first">First panel</Tabs.Panel>
+  //     <Tabs.Panel value="second">Second panel</Tabs.Panel>
+  //   </Tabs>
+  // );
+
   return (
-    <Tabs defaultValue={editors[0].id}>
+    <Tabs value={activeTab} onChange={(t) => handleTabChange(t)}>
       {/* <ScrollArea scrollbarSize={0} style={{ width: "100%", display: "flex" }}> */}
-      <div>
-        <Tabs.List
-          className="editor-tab-list"
-          style={{ display: "flex", flexWrap: "nowrap", alignItems: "center" }}
-        >
-          {editors.map((editor) => (
-            <Tabs.Tab
-              key={editor.id}
-              w={140}
-              px={4}
-              value={editor.id}
-              className="editor-tab"
-              rightSection={
-                <IconX stroke={1} className="editor-tab-close-icon" />
-              }
-            >
-              Query 1
-            </Tabs.Tab>
-          ))}
-          <ActionIcon variant="transparent">
-            <IconPlus size={20} stroke={1} />
-          </ActionIcon>
-        </Tabs.List>
-      </div>
+      <Tabs.List
+        className="editor-tab-list"
+        style={{ display: "flex", flexWrap: "nowrap", alignItems: "center" }}
+      >
+        {editors.map((editor) => (
+          <Tabs.Tab
+            key={editor.id}
+            w={140}
+            px={4}
+            value={editor.id}
+            className="editor-tab"
+            rightSection={
+              <IconX stroke={1} className="editor-tab-close-icon" />
+            }
+          >
+            Query 1
+          </Tabs.Tab>
+        ))}
+        <ActionIcon variant="transparent" onClick={handleAddEditor}>
+          <IconPlus size={20} stroke={1} />
+        </ActionIcon>
+      </Tabs.List>
       {/* </ScrollArea> */}
 
       {editors.map((editor) => (
